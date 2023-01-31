c52063b0b4e6d89e815aeafa4014d480a548c92c
rclark848 <rclark848@gmail.com>
2023-01-31 11:59:32 -0500

    new maps with rug plot and transparency

--- scripts/maps.R
+++ scripts/maps.R
@@ -9,39 +9,37 @@ library(maps)
 library(mapdata)
 library(tidyverse)
 
-msat <- read.csv("output/latlon_msat_2020-12-16.csv")
-mtdna <- read.csv("output/latlon_mtdna_2020-10-20.csv")
-merged <- rbind(msat, mtdna)
-unique_merged <- unique(merged)
-
 #read in data
-msat <- read.csv("output/msat_assembled_2.csv")
-mtdna <- read.csv("output/mtdna_assembled_2.csv")
+msat <- read.csv("output/latlon_msat_2023-01-31.csv")
+mtdna <- read.csv("output/latlon_mtdna_2023-01-31.csv")
+merged <- rbind(msat, mtdna)
+  unique_merged <- unique(merged)
 
 #pull world map data
-geogr_data <- map_data('world')
+world_map <- map_data("world") %>% 
+  filter(! long > 180)
 
 ################################################################################################################################################
 
 ######## Create msat map ########
 
-msat_plot <- ggplot(geogr_data, aes(x = long, y = lat, group = group)) + 
-  geom_polygon(fill = "lightgray", colour = "white") + 
-  geom_point(data = msat, aes(x = lon, y = lat), size = 4, inherit.aes = FALSE)
-#msat_plot
-msat_plot_annotated <- msat_plot + xlab("Longitude (°)") + ylab("Latitude (°)") + theme_bw() + 
-  ggtitle("msat locations") + 
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(), 
-        axis.line = element_line(size = 1), axis.ticks = element_line(color = "black", size = 1), 
-        axis.text = element_text(size = 20, color = "black"), axis.title = element_text(size = 24, face = "bold"), 
-        plot.title = element_text(size = 28, face = "bold"))
+msat_plot <- world_map %>% 
+  ggplot(aes(map_id = region)) +
+  geom_map(map = world_map, color = "lightgray", fill = "lightgray") +
+  expand_limits(x = world_map$long, y = world_map$lat) +  
+  #ggplot() + 
+  geom_point(data = msat, aes(x = lon, y = lat), color = "#626c76", alpha = 0.5, size = 8, 
+             inherit.aes = FALSE) + 
+  geom_rug(data = msat, mapping = aes(x = lon), color = "#282828", alpha = 0.5, inherit.aes = FALSE) + 
+  geom_rug(data = msat, mapping = aes(y = lat), color = "#282828", alpha = 0.5, inherit.aes = FALSE)
+msat_plot_annotated <- msat_plot + ylim(c(-90, 90)) + theme_map()
 msat_plot_annotated
 
 ######## Create mtdna map ########
 
-mtdna_plot <- ggplot(geogr_data, aes(x = long, y = lat, group = group)) + 
+mtdna_plot <- ggplot(world_map, aes(x = long, y = lat)) + 
   geom_polygon(fill = "lightgray", colour = "white") + 
-  geom_point(data = mtdna, aes(x = lon, y = lat), size = 4, inherit.aes = FALSE)
+  geom_point(data = mtdna, aes(x = lon, y = lat), size = 4, inherit.aes = FALSE) + geom_rug(data = mtdna)
 #mtdna_plot
 mtdna_plot_annotated <- mtdna_plot + xlab("Longitude (°)") + ylab("Latitude (°)") + theme_bw() + 
   ggtitle("mtdna locations") + 
@@ -53,69 +51,29 @@ mtdna_plot_annotated
 
 ######## Create mtdna map ########
 
-mtdna_plot <- ggplot(geogr_data, aes(x = long, y = lat, group = group)) + 
-  geom_polygon(fill = "lightgray", colour = "white") + 
-  geom_point(data = mtdna, aes(x = lon, y = lat), size = 4, inherit.aes = FALSE)
-#mtdna_plot
-mtdna_plot_annotated <- mtdna_plot + xlab("Longitude (°)") + ylab("Latitude (°)") + theme_bw() + 
-  ggtitle("mtdna locations") + 
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(), 
-        axis.line = element_line(size = 1), axis.ticks = element_line(color = "black", size = 1), 
-        axis.text = element_text(size = 20, color = "black"), axis.title = element_text(size = 24, face = "bold"), 
-        plot.title = element_text(size = 28, face = "bold"))
+mtdna_plot <- world_map %>% 
+  ggplot(aes(map_id = region)) +
+  geom_map(map = world_map, color = "lightgray", fill = "lightgray") +
+  expand_limits(x = world_map$long, y = world_map$lat) +  
+  #ggplot() + 
+  geom_point(data = mtdna, aes(x = lon, y = lat), color = "#626c76", alpha = 0.5, size = 8, 
+             inherit.aes = FALSE) + 
+  geom_rug(data = mtdna, mapping = aes(x = lon), color = "#282828", alpha = 0.5, inherit.aes = FALSE) + 
+  geom_rug(data = mtdna, mapping = aes(y = lat), color = "#282828", alpha = 0.5, inherit.aes = FALSE)
+mtdna_plot_annotated <- mtdna_plot + ylim(c(-90, 90)) + theme_map()
 mtdna_plot_annotated
 
 ######## Create merged map ########
 
-merged_plot <- ggplot(geogr_data, aes(x = long, y = lat, group = group)) + 
-  geom_polygon(fill = "lightgray", colour = "white") + 
-  geom_point(data = unique_merged, aes(x = lon, y = lat), size = 2, inherit.aes = FALSE)
-#merged_plot
-merged_plot_annotated <- merged_plot + xlab("Longitude (°)") + ylab("Latitude (°)") + theme_bw() + 
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(), 
-        axis.line = element_line(size = 1), axis.ticks = element_line(color = "black", size = 1), 
-        axis.text = element_text(size = 20, color = "black"), axis.title = element_text(size = 24, face = "bold"), 
-        plot.title = element_text(size = 28, face = "bold"))
+merged_plot <- world_map %>% 
+  ggplot(aes(map_id = region)) +
+  geom_map(map = world_map, color = "lightgray", fill = "lightgray") +
+  expand_limits(x = world_map$long, y = world_map$lat) +  
+  #ggplot() + 
+  geom_point(data = merged, aes(x = lon, y = lat), color = "#626c76", alpha = 0.5, size = 8, 
+             inherit.aes = FALSE) + 
+  geom_rug(data = merged, mapping = aes(x = lon), color = "#282828", alpha = 0.5, inherit.aes = FALSE) + 
+  geom_rug(data = merged, mapping = aes(y = lat), color = "#282828", alpha = 0.5, inherit.aes = FALSE)
+merged_plot_annotated <- merged_plot + ylim(c(-90, 90)) + theme_map()
 merged_plot_annotated
 
-##########################################################################################################
-
-######## Create msat histograms ########
-
-#lat histogram
-msat_lat_histogram <- ggplot(data = msat, aes(msat$lat)) + geom_histogram(binwidth = 10, color = "#00B8E5", fill = "#00B8E5") + 
-  xlab("latitude") + ylab("count")
-msat_lat_histogram_annotated <- msat_lat_histogram + theme_bw() + 
-  scale_x_continuous(limits = c(-80, 80), breaks = c(-80, -60, -40, -20, 0, 20, 40, 60, 80)) + coord_flip() + 
-  theme(panel.border = element_rect(size = 1), axis.title = element_text(size = 40, face = "bold"), 
-        axis.ticks = element_line(color = "black", size = 1), axis.text = element_text(size = 40, color = "black"))
-msat_lat_histogram_annotated
-
-#lon histogram
-msat_lon_histogram <- ggplot(data = msat, aes(msat$lon)) + geom_histogram(binwidth = 10, color = "#00B8E5", fill = "#00B8E5") + 
-  xlab("longitude") + ylab("count")
-msat_lon_histogram_annotated <- msat_lon_histogram + theme_bw() + 
-  scale_x_continuous(limits = c(-190, 180), breaks = c(-180, -160, -140, -120, -100, -80, -60, -40, -20, 0, 20, 40, 60, 80, 100, 120, 140, 160, 180)) +
-  theme(panel.border = element_rect(size = 1), axis.title = element_text(size = 40, face = "bold"), 
-        axis.ticks = element_line(color = "black", size = 1), axis.text = element_text(size = 40, color = "black"))
-msat_lon_histogram_annotated
-
-######## Create mtdna histograms ########
-
-#lat histogram
-mtdna_lat_histogram <- ggplot(data = mtdna, aes(mtdna$lat)) + geom_histogram(binwidth = 10, color = "#00B8E5", fill = "#00B8E5") + 
-  xlab("latitude") + ylab("count")
-mtdna_lat_histogram_annotated <- mtdna_lat_histogram + theme_bw() + 
-  scale_x_continuous(limits = c(-80, 80), breaks = c(-80, -60, -40, -20, 0, 20, 40, 60, 80)) + coord_flip() + 
-  theme(panel.border = element_rect(size = 1), axis.title = element_text(size = 40, face = "bold"), 
-        axis.ticks = element_line(color = "black", size = 1), axis.text = element_text(size = 40, color = "black"))
-mtdna_lat_histogram_annotated
-
-#lon histogram
-mtdna_lon_histogram <- ggplot(data = mtdna, aes(mtdna$lon)) + geom_histogram(binwidth = 10, color = "#00B8E5", fill = "#00B8E5") + 
-  xlab("longitude") + ylab("count")
-mtdna_lon_histogram_annotated <- mtdna_lon_histogram + theme_bw() + 
-  scale_x_continuous(limits = c(-190, 180), breaks = c(-180, -160, -140, -120, -100, -80, -60, -40, -20, 0, 20, 40, 60, 80, 100, 120, 140, 160, 180)) +
-  theme(panel.border = element_rect(size = 1), axis.title = element_text(size = 40, face = "bold"), 
-        axis.ticks = element_line(color = "black", size = 1), axis.text = element_text(size = 40, color = "black"))
-mtdna_lon_histogram_annotated
